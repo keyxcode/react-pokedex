b9845cfa89a0a4148df9eb4d777fa59b3a152951
keyxcode <keyxcode@gmail.com>
2023-04-29 02:01:27 +0700

    Add buttons and forms to change id

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [pokemonId, setPokemonId] = useState(1);
   const [pokemonData, setPokemonData] = useState("");
   const [pokemonImg, setPokemonImg] = useState("");
+  const [searchId, setSearchId] = useState("");
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
@@ -15,23 +16,36 @@ function App() {
         )
       );
 
-    console.log(pokemonImg);
-
     fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`)
       .then((response) => response.json())
       .then((data) =>
         setPokemonData(data["flavor_text_entries"][0]["flavor_text"])
-      )
-      .catch((e) => console.log(e));
-    setPokemonId(1);
-  }, []);
+      );
+  }, [pokemonId]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPokemonId(searchId);
+    setSearchId("");
+  };
 
   return (
     <div>
       <div className="pokedex-outline">
         <img src={pokemonImg} />
         <div>{pokemonData}</div>
+        <div>#{pokemonId}</div>
       </div>
+      <button onClick={() => setPokemonId(pokemonId - 1)}>Prev</button>
+      <button onClick={() => setPokemonId(pokemonId + 1)}>Next</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchId}
+          onChange={(e) => setSearchId(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
