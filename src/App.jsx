90a30c496cf1545caa11d91b22e00a9e0b57c89b
keyxcode <keyxcode@gmail.com>
2023-04-29 20:06:46 +0700

    Implement cycling info zone

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [pkmDescription, setPkmDescription] = useState("");
   const [pkmInfo, setPkmInfo] = useState("");
   const [searchId, setSearchId] = useState("");
+  const [activeInfoSlide, setActiveInfoSlide] = useState(0);
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
@@ -19,6 +20,18 @@ function App() {
       );
   }, [pokemonId]);
 
+  useEffect(() => {
+    const slides = document.querySelectorAll(".info-slide");
+    const slidesLength = slides.length;
+
+    for (let i = 0; i < slidesLength; ++i) {
+      if (i === activeInfoSlide) slides[i].style.display = "block";
+      else {
+        slides[i].style.display = "none";
+      }
+    }
+  }, [activeInfoSlide]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setPokemonId(parseInt(searchId));
@@ -29,7 +42,37 @@ function App() {
     ? pkmInfo["sprites"]["other"]["official-artwork"]["front_default"]
     : "";
   const name = pkmInfo ? pkmInfo["name"] : "";
+  const types = pkmInfo
+    ? pkmInfo["types"].map((slot) => slot["type"]["name"])
+    : [];
+  const stats = pkmInfo
+    ? pkmInfo["stats"].map((stat) => ({
+        [stat["stat"]["name"]]: stat["base_stat"],
+      }))
+    : [];
 
+  const prevPkm = () => {
+    setPokemonId(pokemonId - 1);
+  };
+  const nextPkm = () => {
+    setPokemonId(pokemonId + 1);
+  };
+
+  const prevSlide = () => {
+    const slides = document.querySelectorAll(".info-slide");
+    const currentSlide =
+      activeInfoSlide - 1 < 0 ? slides.length - 1 : activeInfoSlide - 1;
+
+    setActiveInfoSlide(currentSlide);
+  };
+
+  const nextSlide = () => {
+    const slides = document.querySelectorAll(".info-slide");
+    const currentSlide =
+      activeInfoSlide + 1 >= slides.length ? 0 : activeInfoSlide + 1;
+
+    setActiveInfoSlide(currentSlide);
+  };
   return (
     <div>
       <div className="pokedex-outline">
@@ -38,10 +81,24 @@ function App() {
           #{pokemonId} {name}
         </div>
         <div className="lower-zone">
-          <div className="description-zone">{pkmDescription}</div>
+          <div className="description-zone">
+            <div className="info-slide">
+              {types.map((type) => (
+                <span key={type}>{type} </span>
+              ))}
+            </div>
+            <div className="info-slide">{pkmDescription}</div>
+            <div className="info-slide">
+              {stats.map((stat) => (
+                <div key={JSON.stringify(stat)}>{JSON.stringify(stat)}</div>
+              ))}
+            </div>
+          </div>
           <div className="buttons-zone">
-            <button onClick={() => setPokemonId(pokemonId - 1)}>Prev</button>
-            <button onClick={() => setPokemonId(pokemonId + 1)}>Next</button>
+            <button onClick={prevPkm}>Prev</button>
+            <button onClick={nextPkm}>Next</button>
+            <button onClick={prevSlide}>Down</button>
+            <button onClick={nextSlide}>Up</button>
           </div>
           <form onSubmit={handleSubmit}>
             <input
