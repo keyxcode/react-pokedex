51d01274d095e10faa59715ea082bd9ab0b97edc
keyxcode <keyxcode@gmail.com>
2023-05-04 18:06:07 +0700

    Create global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { createGlobalStyle } from "styled-components";
+import { COLORS, SIZES } from "./utils/constants";
 
 function App() {
   const [pokemonId, setPokemonId] = useState(
@@ -101,8 +103,35 @@ function App() {
     setTimeout(() => light.classList.remove("active-light"), 200);
   };
 
+  const GlobalStyles = createGlobalStyle`
+  * {
+  box-sizing: border-box;
+  font-family: "Silkscreen", system-ui, -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+    sans-serif;
+
+  --red: #d3242b;
+  --dark-red: #ac2933;
+  --light-red: #ee1825;
+  --green: #00a851;
+  --yellow: #fef503;
+  --blue: #01aeec;
+  --black: #1a1619;
+  --dark-gray: #58585a;
+  --light-gray: #f2f3f3;
+
+  --xxs: 1px;
+  --xs: 4px;
+  --s: 8px;
+  --md: 16px;
+  --l: 24px;
+  --xxl: 48px;
+}
+`;
+
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <div className="pokedex-outline">
         <div className="lights-zone">
           <div className="light main-light"></div>
@@ -176,7 +205,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+const COLORS = {
+  red: "#d3242b",
+  darkRed: "#ac2933",
+  lightRed: "#ee1825",
+  green: "#00a851",
+  yellow: "#fef503",
+  blue: "#01aeec",
+  black: "#1a1619",
+  darkGray: "#58585a",
+  lightGray: "#f2f3f3",
+};
+
+const SIZES = {
+  xxs: "1px",
+  xs: "4px",
+  s: "8px",
+  md: "16px",
+  l: "24px",
+  xxl: "48px",
+};
+
+export { COLORS, SIZES };
